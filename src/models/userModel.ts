b47f651cd4599d8fae2dde1d51ca4ba21fdc2ffd
Thomas Dostilio <tdostilio@gmail.com>
2024-05-31 09:56:49 -0400

    updates user schema and interface

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,17 +1,30 @@
 import mongoose, { Document, Schema } from 'mongoose'
 
 export interface IUser extends Document {
-  firstName: string
-  lastName: string
-  email: string
-  password: string
+  email: string,
+  providers: {
+    googleId?: string,
+    githubId?: string,
+    magicLinkId?: string
+  },
+  name?: string,
+  avatar?: string
 }
 
 const UserSchema: Schema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  providers: {
+    googleId: String,
+    githubId: String,
+    magicLinkId: String
+  },
+  name: String,
+  avatar: String,
+  // other fields as needed
 })
 
 export default mongoose.model<IUser>('User', UserSchema)
