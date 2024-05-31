77d9507ed1540e73512ad1157974a6e2d59e1971
Thomas Dostilio <tdostilio@gmail.com>
2024-05-31 10:06:43 -0400

    WIP: adds types for passport, adds wip code for google oauth2.0

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,4 +1,29 @@
 import User, { IUser } from '../models/userModel'
+import passport from 'passport'
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback',
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id })
+
+      if (existingUser) {
+        return done(null, existingUser)
+      }
+
+      const user = await new User({ googleId: profile.id }).save()
+      done(null, user)
+    }
+  )
+)
 
 export class UserService {
   async createUser(firstName: string, lastName: string, email: string, password: string) {
