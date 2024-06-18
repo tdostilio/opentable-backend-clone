504ba34edc7473d0910d9b91e256177e59c4a25e
Thomas Dostilio <tdostilio@gmail.com>
2024-06-18 09:09:15 -0400

    gets token decode working

--- src/services/magicLinkService.ts
+++ src/services/magicLinkService.ts
@@ -2,6 +2,7 @@ import { Strategy } from 'passport-custom'
 import { handleMagicLinkCallback } from '../middleware/authMiddleware'
 import jwt from 'jsonwebtoken'
 
+
 class MagicLinkService extends Strategy {
   constructor() {
     // this is the magic link strategy after a user clicks the email link
@@ -18,8 +19,12 @@ class MagicLinkService extends Strategy {
     if (!process.env.JWT_SECRET_KEY) throw new Error('Unable to verify token')
     try {
       // jwt token returns string if payload is string, object if payload is object
-      const email = jwt.verify(token, process.env.JWT_SECRET_KEY) as string
-      return email
+      const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+      if (typeof decoded === 'object' && 'email' in decoded) {
+        return decoded.email
+      } else {
+        throw new Error('Token payload is not an object or missing email')
+      }
     } catch (err) {
       console.error(err)
       throw new Error('Invalid or expired token')
